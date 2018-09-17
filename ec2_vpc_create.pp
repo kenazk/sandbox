1e2f7df8e59159ef9f46d5dbddd04ed2cd903327
Kenaz <kenaz@puppetlabs.com>
2018-09-17 11:42:57 -0700

    Test commit

---
+++ ec2_vpc_create.pp
@@ -0,0 +1,14 @@
+genesis::aws::vpc { 'nyx-vpc-test':
+  ensure               => present,
+  region               => "eu-west-1",
+  cidr_block           => "10.90.0.10/24",
+  tags => {
+      'created_by' => 'john.mccabe@puppet.com',
+      'department' => 'engineering',
+      'project' => 'incubator',
+      'lifetime' => '1h'
+  },
+  enable_dns_hostnames => true,
+  enable_dns_support   => true,
+} ->
+notice("Created VPC: ${resource(Genesis::Aws::Vpc['nyx-vpc-test']).vpc_id}\n")
